2bd29bac18aa077067b489e594130a9c057a05a1
Enisz Abdalla <enisz87@gmail.com>
2022-05-03 16:52:54 +0200

    updated trigger method

--- src/api/Pipelines.ts
+++ src/api/Pipelines.ts
@@ -68,7 +68,7 @@ export class Pipelines extends AbstractApiEndpoint {
             }
         }
 
-        return this.getAxios().post(`projects/${id}/trigger/pipeline`, formData, { headers: formData.getHeaders() })
+        return this.getAxios().post(`projects/${id}/trigger/pipeline`, formData, { headers: formData.getHeaders() ? formData.getHeaders() : undefined })
             .then((response: AxiosResponse<TriggeredPipeline>) => response.data);
     }
 
